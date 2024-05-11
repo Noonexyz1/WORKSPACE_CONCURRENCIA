0557d2d4ed422288379e3f4eaf207a707c6ccbb3
Noonexyz1 <aries-d2github@hotmail.com>
2024-05-11 16:36:25 -0400

    syncronized-3 agregado syncronized en el metodo

---
+++ synchronized-3/src/main/java/org/hilos/Main.java
@@ -0,0 +1,47 @@
+package org.hilos;
+
+public class Main {
+
+    // valor critico GLOBAL
+    private int contador = 0;
+
+    public synchronized void realizarTarea() {
+        //variable local para cada hilo
+        int i;
+        for (i = 0; i < 1000; i++) {
+            // valor GLOBAL y proceso seccion CRITICO
+            contador++;
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public int obtenerContador() {
+        return this.contador;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Main m1 = new Main();
+        System.out.println("Contador inicial: " + m1.obtenerContador());
+
+        Thread t1 = new Thread(m1::realizarTarea);
+        Thread t2 = new Thread(m1::realizarTarea);
+
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Contador despues: " + m1.obtenerContador());
+
+
+    }
+    
+}
